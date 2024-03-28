454c7fa404918d844ec43a5090307e23fa4c3e31
erp-shamim <multibrandworkshop.ccdbill@gmail.com>
2024-03-28 15:52:38 +0600

    new code calculation

---
+++ Variable_Data_Types/Calc/calculation.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<math.h>
+
+int main(){
+    int a, b;
+    printf("Input a:");
+    scanf("%d", &a);
+
+    printf("Input b:");
+    scanf("%d", &b);
+
+    int result = a+b;
+    printf("Answer is = %d", result);
+
+    return 0;
+
+}
