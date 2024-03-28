ceeb652a14e291dbbf600af6dadf6505a2eb2488
erp-shamim <multibrandworkshop.ccdbill@gmail.com>
2024-03-28 16:37:11 +0600

    Properly Calculation Done

--- Variable_Data_Types/Calc/calculation.c
+++ Variable_Data_Types/Calc/calculation.c
@@ -8,9 +8,21 @@ int main(){
 
     printf("Input b:");
     scanf("%d", &b);
-
+    // add two intiger values
     int result = a+b;
-    printf("Answer is = %d", result);
+    printf("Answer Addition is = %d", result);
+    
+    // sub two intiger values
+    int result1 = a-b;
+    printf("\nAnswer Subtraction is = %d", result1);
+
+    // multiply two intiger values
+    int result2 = a*b;
+    printf("\nAnswer Multiplied is = %d", result2);
+
+    // Division two intiger values
+    int result3 = a/b;
+    printf("\nAnswer Divided is = %d", result3);
 
     return 0;
 
